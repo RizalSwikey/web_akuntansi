a3f408d310eb195e37bc00eaef16a3da67c147a3
ersaasyhar <ersa.asyhar@gmail.com>
2025-10-20 22:05:58 +0700

    feat(route): integrate and refactor

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'core'
+
 urlpatterns = [
     path('', views.landing_page_view, name='landing_page'),
     path('profile/', views.profile_view, name='profile'),
+    path('pendapatan/', views.pendapatan_view, name='pendapatan'),
+    path('hpp/', views.hpp_view, name='hpp'),
+    path('beban-usaha/', views.beban_usaha_view, name='beban_usaha'),
+    path('laporan/', views.laporan_view, name='laporan'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 
+
 def landing_page_view(request):
     """
     landing page
@@ -13,3 +14,38 @@ def profile_view(request):
     profile page
     """
     return render(request, 'core/pages/profile.html')
+
+
+def dashboard_view(request):
+    """
+    dashboard page
+    """
+    return render(request, 'core/pages/dashboard.html')
+
+
+def pendapatan_view(request):
+    """
+    pendapatan page
+    """
+    return render(request, 'core/pages/pendapatan.html')
+
+
+def hpp_view(request):
+    """
+    hpp page
+    """
+    return render(request, 'core/pages/hpp.html')
+
+
+def beban_usaha_view(request):
+    """
+    beban usaha page
+    """
+    return render(request, 'core/pages/beban_usaha.html')
+
+
+def laporan_view(request):
+    """
+    laporan page
+    """
+    return render(request, 'core/pages/laporan.html')
